e86a1c0f7624332912284bbb7c18f7558c653a96
kunal <thekunalbarchha@gmail.com>
2025-06-10 14:47:38 +0530

    email, mobile, personal details | working

---
+++ ToDo.java
@@ -0,0 +1,10 @@
+public class ToDo {
+
+    /*
+    TODO:
+    1. store user sessions in redis
+    2. create /personal response object.
+    3. write scheduler to check User object in mongo and redis. Handle if any mismatch.
+
+     */
+}
