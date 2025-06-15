6abecc9f51e7b1febcf68ed8ad140c79b41ddca1
kunal <thekunalbarchha@gmail.com>
2025-06-15 10:46:07 +0530

    complyCube KYC integration | not tested

--- ToDo.java
+++ ToDo.java
@@ -2,9 +2,14 @@ public class ToDo {
 
     /*
     TODO:
-    1. store user sessions in redis
-    2. create /personal response object.
-    3. write scheduler to check User object in mongo and redis. Handle if any mismatch.
+    1. store user sessions in redis --- Done. Need updates for KYC and AML.
+    2. create /personal response object. -- done.
+    3. write scheduler to check User object in mongo and redis. Handle if any mismatch. --- pending
 
+    4. update User data in KYC, personal/, and POA update.
+    5. Store KYC response from complyCube and other providers for record keeping.
+    6. validate IP at login against whitelisted IP.
+    7. Registration validation -> Check if mobile already registered. Mobile number has to be unique for all users.
+    8. Keep user KYCStatus PENDING as long as documents are not uploaded. When submitted, set INITIATED_BY_USER.
      */
 }
