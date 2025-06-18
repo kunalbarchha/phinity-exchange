d0865aa28bff3adca8c5607b026784ded80dfd64
kunal <thekunalbarchha@gmail.com>
2025-06-18 12:13:57 +0530

    asset collection CRUD | WIP

--- ToDo.java
+++ ToDo.java
@@ -5,11 +5,12 @@ public class ToDo {
     1. store user sessions in redis --- Done. Need updates for KYC and AML.
     2. create /personal response object. -- done.
     3. write scheduler to check User object in mongo and redis. Handle if any mismatch. --- pending
-
-    4. update User data in KYC, personal/, and POA update.
-    5. Store KYC response from complyCube and other providers for record keeping.
+    4. update User data in KYC, personal/, and POA update. -- done
+    5. upload user documents to S3.
+    5. Store KYC response from complyCube and other providers for record keeping. -- done
     6. validate IP at login against whitelisted IP.
     7. Registration validation -> Check if mobile already registered. Mobile number has to be unique for all users.
     8. Keep user KYCStatus PENDING as long as documents are not uploaded. When submitted, set INITIATED_BY_USER.
+    9. Strict security for communication through Kafka
      */
 }
